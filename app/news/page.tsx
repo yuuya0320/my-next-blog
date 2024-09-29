ad62415999ae1c263f97e076c15794f4326a91d5
yuuya <apple@appurunoMacBook-puro.local>
2024-09-29 09:43:03 +0900

    ニュースにISRを適用

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -4,7 +4,7 @@ import NewsList from "@/app/_components/NewsList";
 import Pagination from "@/app/_components/Pagination";
 import SearchField from "@/app/_components/SearchField";
 
-export const revalidate = 0;
+export const revalidate = 60;
 
 export default async function Page() {
   const { contents: news, totalCount } = await getNewsList({
