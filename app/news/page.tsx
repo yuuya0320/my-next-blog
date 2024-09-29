c4956b81f052765a1f68e7b1611b6dcde27d3608
yuuya <apple@appurunoMacBook-puro.local>
2024-09-29 09:38:24 +0900

    ニュースにSSRを適用

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -4,6 +4,8 @@ import NewsList from "@/app/_components/NewsList";
 import Pagination from "@/app/_components/Pagination";
 import SearchField from "@/app/_components/SearchField";
 
+export const revalidate = 0;
+
 export default async function Page() {
   const { contents: news, totalCount } = await getNewsList({
     limit: NEWS_LIST_LIMIT,
