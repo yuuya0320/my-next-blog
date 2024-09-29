e58ef46c2f1aac29b2da41b5c5a39117bada89f0
yuuya <apple@appurunoMacBook-puro.local>
2024-09-29 10:40:46 +0900

    8章まで完了

--- middleware.ts
+++
@@ -1,7 +0,0 @@
-import { createNextAuthMiddleware } from "nextjs-basic-auth-middleware";
-
-export const middleware = createNextAuthMiddleware();
-
-export const config = {
-  matcher: ["/(/*)"],
-};
